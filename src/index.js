daefe300618f22e2347183d648fab3a034cfff36
Marco Salazar de Leon <msalazardeleon@gmail.com>
2017-02-02 17:44:28 -0800

    Renamed method and modified options for parent function

--- src/index.js
+++ src/index.js
@@ -5,38 +5,40 @@ const parseInfo = require('./parseInfo');
 
 module.exports = (options) => {
   // get our options values
-  const { hosts, infoKeys, callback } = options;
-
-  function fetchInfo(host) {
-    const redis = new Redis({ host });
-
-    redis.info((err, info) => {
-      // Set the record object
-      const record = {
-        timestamp: moment().startOf('minute').toISOString(),
-        host,
-      };
-
-      if (err) {
-        try {
-          record.error = err.toString();
-        } catch (e) {
-          record.error = e.toString();
-        }
-      }
-
-      // We are calling the callback with null and the merged record
-      // with the parsed info by the keys
-      callback(null, Object.assign(record, parseInfo(info, infoKeys)));
-
-      // Disconnect our Redis instance
-      redis.disconnect();
-    });
-  }
+  const { instances } = options;
 
   return {
-    monitor: () => {
-      hosts.forEach(fetchInfo);
+    getInfo: (keys, callback) => {
+      // Iterate over all the instances
+      instances.forEach(({ host, label, description }) => {
+        const redis = new Redis({ host });
+
+        redis.info((err, info) => {
+          // Set the record object
+          const record = {
+            timestamp: moment().startOf('minute').toISOString(),
+            host,
+            label,
+            description,
+          };
+
+          if (err) {
+            try {
+              record.error = err.toString();
+            } catch (e) {
+              record.error = e.toString();
+            }
+          }
+
+          // We are calling the callback with null and the merged record
+          // with the parsed info by the keys
+          callback(null, Object.assign(record, parseInfo(info, keys)));
+
+          // Disconnect our Redis instance
+          redis.disconnect();
+        });
+      });
+
       return this;
     },
   };
